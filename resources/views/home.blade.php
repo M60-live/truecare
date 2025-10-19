87bf705221ec1c81ccd4656f006b16741aad074d
Sipho Maswanganye <38434202+M60-live@users.noreply.github.com>
2025-10-19 21:06:50 +0200

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>TrueCare</title>
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=quicksand:300,500,700" rel="stylesheet" />
+        <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+    </head>
+    <body>
+        <img src="{{ '/img/TrueCare-trans-tri.png' }}" style="height:auto; width:250px;" />
+        <p>Font loaded: Taking Care of your oral and well-being.</p>
+    </body>
+</html>
