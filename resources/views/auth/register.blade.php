62e074da2098fd8a4fc9abf6ddc89a4c129e874b
Sipho Maswanganye <38434202+M60-live@users.noreply.github.com>
2025-12-02 20:15:57 +0200

    Register Account Blade

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,3 @@
-{{--
 @extends('layout.layout')
 @section('content')
 
@@ -9,40 +8,27 @@
                     @csrf
 
                     <!-- Name -->
-                    <div>
-                        <x-input-label for="name" :value="__('Name')" />
-                        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                    <div class="form-floating mb-3">
+                        <input type="text" name="name" class="form-control" id="name" placeholder="Full Name" required autofocus />
+                        <label for="name">name</label>
                     </div>
 
                     <!-- Email Address -->
-                    <div class="mt-4">
-                        <x-input-label for="email" :value="__('Email')" />
-                        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    <div class="form-floating mb-3">
+                        <input type="email" name="email" class="form-control" id="email" placeholder="Email" required />
+                        <label for="email">Email</label>
                     </div>
 
                     <!-- Password -->
-                    <div class="mt-4">
-                        <x-input-label for="password" :value="__('Password')" />
-
-                        <x-text-input id="password" class="block mt-1 w-full"
-                                        type="password"
-                                        name="password"
-                                        required autocomplete="new-password" />
-
-                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    <div class="form-floating mb-3">
+                        <input type="password" name="password" class="form-control" id="password" placeholder="Password" required />
+                        <label for="password">Password</label>
                     </div>
 
                     <!-- Confirm Password -->
-                    <div class="mt-4">
-                        <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                        <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                        type="password"
-                                        name="password_confirmation" required autocomplete="new-password" />
-
-                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                    <div class="form-floating mb-3">
+                        <input type="password" name="password_confirmation" class="form-control" id="password_confirmation" placeholder="Confirm Password" required />
+                        <label for="password_confirmation">Confirm Password</label>
                     </div>
 
                     <div class="flex items-center justify-end mt-4">
@@ -50,13 +36,10 @@
                             {{ __('Already registered?') }}
                         </a>
 
-                        <x-primary-button class="ms-4">
-                            {{ __('Register') }}
-                        </x-primary-button>
+                        <button type="submit" class="btn btn-dark primary-button ms-3">Register <span class="fa fa-arrow-circle-right"></span></button>
                     </div>
                 </form>
             </div>
         </div>
     </section>
 @endsection
---}}
